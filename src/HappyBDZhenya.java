9e312d5e2585126f69a8e38848c32f2c589d9bc5
astonuser <YArtemkina@alfabank.ru>
2025-02-18 17:50:31 +0300

    Для Жени

---
+++ src/HappyBDZhenya.java
@@ -0,0 +1,46 @@
+public class HappyBDZhenya {
+
+    public static void main(String[] args) {
+
+        System.out.println(" HHH     HHH        AA         PPPPPPPPPPP  PPPPPPPPPPP    YY         YY ");
+        System.out.println(" HHH     HHH        AA         PPPPPPPPPPP  PPPPPPPPPPP    YY         YY ");
+        System.out.println(" HHH     HHH       AAAA        PP       PP  PP       PP     YY       YY ");
+        System.out.println(" HHH     HHH      AA  AA       PP       PP  PP       PP      YY     YY  ");
+        System.out.println(" HHHHHHHHHHH     AA    AA      PPPPPPPPPPP  PPPPPPPPPPP       YYYYYYY ");
+        System.out.println(" HHH     HHH    AAAAAAAAAA     PP           PP                    YY ");
+        System.out.println(" HHH     HHH   AA        AA    PP           PP                   YY ");
+        System.out.println(" HHH     HHH  AA          AA   PP           PP                  YY ");
+
+        System.out.println("BBBBBBBBBBB  IIIIIIIIIIIII RRRRRRRRRRRR  TTTTTTTTTTTTTT HHHH       HHHH  DDDDDDDDDDD        AA   YY          YY ");
+        System.out.println("BB        BB      II       RR        RR        TT       HHHH       HHHH  DD         D      AAAA   YY        YY ");
+        System.out.println("BB        BB      II       RR       RR         TT       HHHH       HHHH  DD          D    AA  AA   YY      YY ");
+        System.out.println("BBBBBBBBBB        II       RRRRRRRRRRR         TT       HHHHHHHHHHHHHHH  DD          D   AA    AA   YYYYYYYY ");
+        System.out.println("BB        BB      II       RR       RR         TT       HHHH       HHHH  DD          D  AAAAAAAAAA       YY ");
+        System.out.println("BB        BB      II       RR        RR        TT       HHHH       HHHH  DD         D  AA        AA     YY  ");
+        System.out.println("BBBBBBBBBBB  IIIIIIIIIIIII RR         RR       TT       HHHH       HHHH  DDDDDDDDDDD  AA          AA   YY ");
+
+        System.out.println("           ZZ       HH   HH       EEEEEEE       NN    NN        YY       YY        A A      ");
+        System.out.println("           ZZ       HH   HH       EE            NN NN  NN        YY     YY        AA AA     ");
+        System.out.println("           ZZ       HH   HH       EE            NN  NN NN         YY   YY        AA   AA    ");
+        System.out.println("           ZZ       HHHHHHH       EEEEE         NN   NNN           YY YY        AAAAAAAAA   ");
+        System.out.println(" ZZ        ZZ       HH   HH       EE            NN    NN            YY         AA       AA  ");
+        System.out.println(" ZZ        ZZ       HH   HH       EE            NN    NN            YY        AA         AA ");
+        System.out.println(" ZZZZZZZZZZZZ       HH   HH       EEEEEEE       NN    NN            YY       AA           AA");
+
+        System.out.println("                       ~~");
+        System.out.println("                       ||");
+        System.out.println("                       ||");
+        System.out.println("                 ~~~~~~~~~~~~~~~~~   ");
+        System.out.println("                |                 |");
+        System.out.println("                |                 |");
+        System.out.println("            ____|                 |___");
+        System.out.println("           |     `````````````````    |");
+        System.out.println("           |     Congratulations!     |");
+        System.out.println("     ______|                          |_____   ");
+        System.out.println("     |      ```````````````````````````     | ");
+        System.out.println("     |       Happy Birtday Evgen!           | ");
+        System.out.println("     |______________________________________|    ");
+
+        System.out.println(" Желаю крепкого здоровья, исполнения желаний, побольше путешествий и много дэнег 💰");
+    }
+}
